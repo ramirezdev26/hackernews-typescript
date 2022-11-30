f4666cdb661107be039b0b9626de293740e1c9c6
santiago ramirez cano <santiago.ramirezc26@gmail.com>
2022-11-30 09:53:59 -0500

    Updated link.ts for voters

--- src/graphql/link.ts
+++ src/graphql/link.ts
@@ -14,6 +14,14 @@ export const Link = objectType({
                 return context.prisma.link
                     .findUnique({ where: { id: parent.id } })
                     .postedBy();
+            },
+        });
+        t.nonNull.list.nonNull.field("voters", {
+            type: "User",
+            resolve(parent, args, context) {
+                return context.prisma.link
+                    .findUnique({ where: { id: parent.id } })
+                    .voters();
             }
         })
     },
