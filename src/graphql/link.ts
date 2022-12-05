fa77b100f36e9910009ce556a6b342c9ce094bfa
santiago ramirez cano <santiago.ramirezc26@gmail.com>
2022-12-05 09:55:20 -0500

    Implementation of sorting in feed query creating a new input type and enum to define the ordering options for sorting. LinkOrderByInput represents the criteria by which that the list of Link elements can be sorted. The Sort enum is used to define the sorting order.

--- src/graphql/link.ts
+++ src/graphql/link.ts
@@ -1,6 +1,6 @@
-import { extendType, intArg, nonNull, objectType, stringArg } from 'nexus';
-import { NexusGenObjects } from '../../nexus-typegen';
-
+import { extendType, intArg, nonNull, objectType, stringArg, inputObjectType, enumType, arg, list } from 'nexus';
+import { Prisma } from "@prisma/client"
+import { argsToArgsConfig } from 'graphql/type/definition';
 
 export const Link = objectType({
     name: 'Link', 
@@ -39,6 +39,7 @@ export const LinkQuery = extendType({
                 filter: stringArg(),
                 skip: intArg(),
                 take: intArg(),
+                orderBy: arg({ type: list(nonNull(LinkOrderByInput)) }),
             },
             resolve(parent, args, context, info) {
                 const where = args.filter
@@ -53,10 +54,25 @@ export const LinkQuery = extendType({
                     where,
                     skip: args?.skip as number | undefined,
                     take: args?.take as number | undefined,
+                    orderBy: args?.orderBy as Prisma.Enumerable<Prisma.LinkOrderByWithRelationInput> | undefined,
                 });
             },
         });
     },
+});
+
+export const LinkOrderByInput = inputObjectType({
+    name: "LinkOrderByInput",
+    definition(t) {
+        t.field("description", { type: Sort });
+        t.field("url", { type: Sort });
+        t.field("createdAt", { type: Sort });
+    },
+});
+
+export const Sort = enumType({
+    name: "Sort",
+    members: ["asc", "desc"],
 });
 
 export const LinkOneQuery = extendType({
